4c6416b577f6a46ea7a37d8056ff24455a6c9ea4
Mateus Ceci <mateusceci@gmail.com>
2025-01-07 10:51:49 -0300

    feat: add joi adapter props

--- src/adapters/joi.ts
+++ src/adapters/joi.ts
@@ -1,6 +1,10 @@
 import { ObjectSchema } from "joi";
 
-export class JoiAdapter {
+export interface JoiAdapterProps {
+  validateSchema(schema: ObjectSchema, data: any): Promise<any>;
+}
+
+export class JoiAdapter implements JoiAdapterProps {
   async validateSchema(schema: ObjectSchema, data: any): Promise<any> {
     return await schema.validateAsync(data);
   }
