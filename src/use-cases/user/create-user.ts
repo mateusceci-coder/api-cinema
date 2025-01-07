7f901fd2e7b9030af2e818d13b141ef14a7ba593
Mateus Ceci <mateusceci@gmail.com>
2025-01-07 10:54:32 -0300

    fix: get correct user interface

--- src/use-cases/user/create-user.ts
+++ src/use-cases/user/create-user.ts
@@ -1,5 +1,15 @@
+import { User } from "../../domain/user";
+import { CreateUserParams } from "../../domain/user";
 import { CreateUserRepository } from "../../repositories/user/create-user";
+
+export interface CreateUserUseCase {
+  handle(createUserParams: CreateUserParams): Promise<User>;
+}
 
 export class CreateUserUseCase {
   constructor(private readonly createUserRepository: CreateUserRepository) {}
+
+  async handle(createUserParams: CreateUserParams): Promise<User> {
+    return await this.createUserRepository.add(createUserParams);
+  }
 }
