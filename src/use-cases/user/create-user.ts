66e8880206c1d0712c184694b54b059ddcf86557
Mateus Ceci <mateusceci@gmail.com>
2025-01-09 09:39:10 -0300

    feat: add EmailAlreadyInUse error

--- src/use-cases/user/create-user.ts
+++ src/use-cases/user/create-user.ts
@@ -1,15 +1,28 @@
 import { User } from "../../domain/user";
 import { CreateUserParams } from "../../domain/user";
+import { EmailAlreadyInUseError } from "../../errors/user";
 import { CreateUserRepository } from "../../repositories/user/create-user";
+import { GetUserByEmailRepository } from "../../repositories/user/get-user-by-email";
 
 export interface CreateUserUseCase {
   handle(createUserParams: CreateUserParams): Promise<User>;
 }
 
 export class CreateUserUseCase {
-  constructor(private readonly createUserRepository: CreateUserRepository) {}
+  constructor(
+    private readonly createUserRepository: CreateUserRepository,
+    private readonly getUserByEmailRepository: GetUserByEmailRepository
+  ) {}
 
   async handle(createUserParams: CreateUserParams): Promise<User> {
+    const user = await this.getUserByEmailRepository.getByEmail(
+      createUserParams.email
+    );
+
+    if (user) {
+      throw new EmailAlreadyInUseError(user.email);
+    }
+
     return await this.createUserRepository.add(createUserParams);
   }
 }
