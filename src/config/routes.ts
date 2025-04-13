87addfcc8b00d8ad83e5dc49c57aa96fb4d645f6
Mateus Ceci <mateusceci@gmail.com>
2025-04-13 17:41:24 -0300

    feat: add movie routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Router } from "express";
 import userRoutes from "../routes/userRoutes";
+import movieRoutes from "../routes/movieRoutes";
 
 export default (app: Express): void => {
   const router = Router();
   userRoutes(router);
+  movieRoutes(router);
   app.use("/api", router);
 };
