b264d43cc7951910b3f2b773f270f471be1c68ba
Mateus Ceci <mateusceci@gmail.com>
2025-04-13 17:41:42 -0300

    feat: add http responses

--- src/controller/helpers/http.ts
+++ src/controller/helpers/http.ts
@@ -2,19 +2,33 @@ interface ResponseBody {
   [key: string]: any;
 }
 
-export interface CreatedResponse {
+export interface HttpResponse {
   statusCode: number;
   body: ResponseBody;
 }
 
-export const created = (body: ResponseBody): CreatedResponse => {
+export const created = (body: ResponseBody): HttpResponse => {
   return {
     statusCode: 201,
     body,
   };
 };
 
-export const badRequest = (body: ResponseBody): CreatedResponse => {
+export const ok = (body: ResponseBody): HttpResponse => {
+  return {
+    statusCode: 200,
+    body,
+  };
+};
+
+export const notFound = (body: ResponseBody): HttpResponse => {
+  return {
+    statusCode: 404,
+    body,
+  };
+};
+
+export const badRequest = (body: ResponseBody): HttpResponse => {
   return {
     statusCode: 400,
     body,
